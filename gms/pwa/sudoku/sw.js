1779611807ae3b6d27e5b0837da778cae1e4f3fd
Y-R-U <161470949+Y-R-U@users.noreply.github.com>
2025-12-04 15:30:40 +1000

    Create sw.js

---
+++ gms/pwa/sudoku/sw.js
@@ -0,0 +1,18 @@
+// sw.js
+const CACHE = "sudoku-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll([
+      "/",                  // caches your index.html
+      "./"                  // some hosts need both
+    ]))
+    .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
